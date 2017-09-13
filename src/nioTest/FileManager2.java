588ac784976a077bf58357723e2ff06e573f9fc0
Dylan Duan <dylan.duan@2gotech.com>
2017-09-13 18:09:50 +0800

    add filechannel test

--- src/nioTest/FileManager2.java
+++ src/nioTest/FileManager2.java
@@ -1,9 +1,17 @@
 package nioTest;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.channels.WritableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -12,13 +20,19 @@ public class FileManager2 {
 	
   public static void main(String args[])
   {
+	  /*
 	  Path path=Paths.get("log/stdout.txt");
 	  try {
 		match(path,".*39=8.*");
 	} catch (IOException e) {
 		
 		e.printStackTrace();
-	}
+	}*/
+	  
+	  //testCopy1();
+	  testCopy2();
+	  
+	  
   }
 	
  	
@@ -36,5 +50,67 @@ public class FileManager2 {
 	  
 	  System.out.println("finished-------------------");
   }
+  
+  
+  
+  
+  public static void testCopy1(){
+	  Path from=Paths.get("log/stdout.txt");
+	  Path to=Paths.get("log/stdoutCopy1.txt");
+	  copy1(from,to);
+  }
+  
+  public static void testCopy2(){
+	  Path from=Paths.get("log/stdout.txt");
+	  Path to=Paths.get("log/stdoutCopy2.txt");
+	  copy2(from,to);
+  }
+  
+  
+  public static void copy1(Path from, Path to){
+	  long startTime=System.nanoTime();
+	  try(BufferedReader reader=Files.newBufferedReader(from);BufferedWriter writer=Files.newBufferedWriter(to);){
+		 String content=null; 
+		 while((content=reader.readLine())!=null){
+			 writer.write(content,0,content.length());
+			 writer.newLine();
+		 }
+		  
+	  }catch(IOException e){
+		  e.printStackTrace();
+	  }
+	  
+	  System.out.println("test copy1 finished, consuming time:"+ ((System.nanoTime()-startTime))/1000000);
+	  
+  }
+  
+  
+  
+  public static void copy2(Path from, Path to){
+	 long startTime=System.nanoTime();
+	 final ByteBuffer buffer =ByteBuffer.allocate(2*1024);
+	 try( ReadableByteChannel rc=Files.newByteChannel(from,StandardOpenOption.READ);WritableByteChannel wc=Files.newByteChannel(to,StandardOpenOption.CREATE,StandardOpenOption.WRITE)){
+		 
+		 while(rc.read(buffer)!=-1){
+			 buffer.flip();
+			 wc.write(buffer);
+			 buffer.compact();
+		 }
+		 
+		 buffer.flip();
+		 while(buffer.hasRemaining()){
+			 wc.write(buffer);
+		 }
+		 
+		 
+	 }catch(IOException e){
+		 e.printStackTrace();
+	 }
+	  
+	  
+	  System.out.println("test copy2 finished, consuming time:"+ ((System.nanoTime()-startTime))/1000000);
+	  
+  }
+  
 
 }
